0c9b9ea1b74c9ed58c1dc1e82e6bd44efce2707a
Mike Morrison <mikemorrison@headsup.health>
2026-05-04 15:57:17 -0700

    Use common tenant for multi-tenant Azure AD auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ const handler = NextAuth({
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_AD_TENANT_ID!,
+      tenantId: 'common',
     }),
   ],
   pages: {
